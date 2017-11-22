20b77d3ed7b44f442fc6e64c85f48798468caeef
Squid49134 <ransom@Udel.edu>
2017-11-22 11:38:22 -0800

    Energy Conservation and Plotting programs

---
+++ GuideFieldRuns/Econ.py
@@ -0,0 +1,90 @@
+import numpy as np
+import matplotlib as mpt
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
+import os
+import sys
+sys.path.append('/global/homes/r/ransom/Py3D')
+from py3d.sub import *
+
+# name_style='unfinished'
+
+
+def get_energy(stdoutPath = None):
+    
+        if stdoutPath is None:
+            stdoutPath = raw_input('Enter path for p3d.stdout file: ')
+            
+        foundData = 0
+        try:
+            stdout = open(stdoutPath, 'r')
+            #fort = open(fortPath, 'r')
+            eTot = []
+            eEM = []
+            eK = []
+            eKe = []
+            eKi = []
+            for line in stdout:
+                if line.find('ENERGY') > -1 and line.find('ENERGY:') < 0:              
+                    eTot.append(float(line.split()[1:2][0]))
+                    eEM.append(float(line.split()[2:3][0]))
+                    eK.append(float(line.split()[3:4][0]))
+                    foundData = 1
+            stdout.close()
+            if foundData == 0:
+                raise Exception
+        except:
+            print('Could not load energies')
+    
+        return np.array(eTot), np.array(eEM), np.array(eK), np.array(eKi), np.array(eKe)
+
+def ECons(Run = None):
+    if Run == None:
+        Run = str(raw_input('Which Run? \n'))
+        num = str(raw_input('How many times has sim restarted? \n'))
+    if int(num) == 0: 
+        pathStdout = '/global/cscratch1/sd/ransom/'+ str(Run) + '/staging/p3d.stdout.00' + num
+        pathToMovieData = '/global/cscratch1/sd/ransom/'+ str(Run) + '/staging'
+        #pathExp = '/global/cscratch1/sd/ransom/' + str(Run) + '/exp3d_' + str(Run)
+        paramFile = 'param_'+ str(Run)
+        paramPath = pathToMovieData + '/' + paramFile
+        
+        param = open(paramPath, 'r')
+        for line in param:
+            if line.find('define') > 0 and line.find('dt') > 0:              
+                dt = float(line.split()[2:3][0])        
+        
+        
+        Energies = get_energy(pathStdout)
+        print('Total Cyclo Times = ' + str(len(Energies[0])*dt))
+        
+        return (100*(Energies[0][-1] - Energies[0][0])/Energies[0][0])*(100/(dt*Energies[0].shape[0]))
+    else:
+        pathStdoutRestart = '/global/cscratch1/sd/ransom/'+ str(Run) + '/staging/p3d.stdout.00' + num
+        pathStdoutOriginal = '/global/cscratch1/sd/ransom/'+ str(Run) + '/staging_00/staging/p3d.stdout.000'
+        EnergiesLength = 0;
+        for i in range(0,int(num)):
+            pathStdout = '/global/cscratch1/sd/ransom/'+ str(Run) + '/staging_0' + str(i) + '/staging/p3d.stdout.00' + str(i)
+            Energy = get_energy(pathStdout)
+            EnergiesLength = EnergiesLength + len(Energy[0])
+        
+        pathToMovieData = '/global/cscratch1/sd/ransom/'+ str(Run) + '/staging'
+        #pathExp = '/global/cscratch1/sd/ransom/' + str(Run) + '/exp3d_' + str(Run)
+        paramFile = 'param_'+ str(Run)
+        paramPath = pathToMovieData + '/' + paramFile
+        
+        param = open(paramPath, 'r')
+        for line in param:
+            if line.find('define') > 0 and line.find('dt') > 0:              
+                dt = float(line.split()[2:3][0])        
+        
+        
+        EnergiesRestart = get_energy(pathStdoutRestart)
+        EnergiesOriginal = get_energy(pathStdoutOriginal)
+        
+        EnergiesLength = EnergiesLength + len(EnergiesRestart[0])
+        print('Total Cyclo Times = ' + str(EnergiesLength*dt))        
+        
+        return (100*(EnergiesRestart[0][-1] - EnergiesOriginal[0][0])/EnergiesOriginal[0][0])*(100/(dt*EnergiesLength))
+    
+print('100 Cyclo dE = ' + str(ECons()))
---
+++ GuideFieldRuns/Plot.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib as mpt
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
+import os
+import sys
+sys.path.append('/global/homes/r/ransom/Py3D')
+from py3d.sub import *
+
+def EPlot():
+    run = raw_input('Which run? \n')
+    
+    restarts = raw_input('How many times has sim been restarted? \n')
+    
+    num = raw_input('Which restart would you like to plot from? \n')
+    
+    if restarts == 0 or num == restarts:
+        movies = '/global/cscratch1/sd/ransom/' + str(run) + '/staging'
+        paramFile = 'param_'+ str(run)   
+        
+        d = load_movie(0, paramFile, movies, ['bz'])   
+    else:
+        movies = '/global/cscratch1/sd/ransom/' + str(run) + '/staging_0' + str(num) + '/staging'
+        paramFile = 'param_'+ str(run)   
+        
+        d = load_movie(0, paramFile, movies, ['bz'])
+    
+    ims(d, 'bz')
+
+#    Energies = get_energy()
+#    file = open(param, 'r')
+#    for line in file:
+#        if line.find('define') > 0 and line.find('dt') > 0:              
+#            dt = float(line.split()[2:3][0])
+#    print('Avg Ki + Ke / K = ' + str(np.average((Energies[3] + Energies[4])/Energies[2] )))
+#    times = np.linspace(0, (Energies[0].shape[0] - 1)*dt, Energies[0].shape[0])
+#    mins = [(100*(Energies[0] - Energies[0][0])/Energies[0][0]).min(), (100*(Energies[1] - Energies[1][0])/Energies[1][0]).min(), (100*(Energies[2] - Energies[2][0])/Energies[2][0]).min(), (100*(Energies[3] - Energies[3][0])/Energies[3][0]).min(), (100*(Energies[4] - Energies[4][0])/Energies[4][0]).min()]
+#    maxs = [(100*(Energies[0] - Energies[0][0])/Energies[0][0]).max(), (100*(Energies[1] - Energies[1][0])/Energies[1][0]).max(), (100*(Energies[2] - Energies[2][0])/Energies[2][0]).max(), (100*(Energies[3] - Energies[3][0])/Energies[3][0]).max(), (100*(Energies[4] - Energies[4][0])/Energies[4][0]).max()]
+#    mins = np.array(mins)
+#    maxs = np.array(maxs)
+#    minTot = mins.min()
+#    maxTot = maxs.max()
+#    
+#    mn = minTot - .1*(maxTot - minTot)
+#    mx = maxTot + .1*(maxTot - minTot) 
+#    
+#    print('E Change at 100 Cyclo Times = ' + str((100*(Energies[0][Energies[0].shape[0] - 1] - Energies[0][0])/Energies[0][0])*(100/(dt*Energies[0].shape[0]))))
+#    
+#    fig1 = plt.figure(1)
+#    fig1.set_size_inches(12, 5, forward = True)
+#    ax1 = fig1.add_subplot(111)
+#    ax1.plot(times, Energies[0], label = 'E Tot')
+#    ax1.plot(times, Energies[1], label = 'E EM')
+#    ax1.plot(times, Energies[2], label = 'E Kin')
+#    ax1.legend(loc='upper right', fontsize = 'large')
+#    ax1.set_xlim([0, Energies[0].shape[0]])
+#    ax1.set_ylim([mins.min() - abs(mins[0] / 4), maxs.max() + abs(mins[0] / 4)])
+#    ax1.set_title('$Energy$' + ' ' + '$Conservation$', fontsize = 20)
+#    ax1.set_ylabel('$Energy$', fontsize = 16)
+#    ax1.set_xlabel('$Movie$' + ' ' + '$Time$', fontsize = 16)
+#    ax1.yaxis.set_label_position('right')
+#    
+#    fig2 = plt.figure(2)
+#    fig2.set_size_inches(12, 5, forward = True)
+#    ax2 = fig2.add_subplot(111)
+#    ax2.plot(times, 100*(Energies[0] - Energies[0][0])/Energies[0][0], 'k', label = 'E Tot', )
+#    ax2.plot(times, 100*(Energies[1] - Energies[1][0])/Energies[1][0], 'r', label = 'E EM')
+#    ax2.plot(times, 100*(Energies[3] - Energies[3][0])/Energies[3][0], 'b', label = 'E Kin i')
+#    ax2.plot(times, 100*(Energies[4] - Energies[4][0])/Energies[4][0], 'g', label = 'E Kin e')
+#    ax2.legend(loc='upper right', fontsize = 'large')
+#    ax2.set_xlim([0, times[times.shape[0] - 1]])
+#    ax2.set_ylim([mn, mx])
+#    ax2.set_title('$Energy$' + ' ' + '$Conservation$', fontsize = 20)
+#    ax2.set_ylabel('$Energy$' + ' ' + '$Percent$' + ' ' + '$Change$', fontsize = 16)
+#    ax2.set_xlabel('$Cyclotron$' + ' ' + '$Times$', fontsize = 16)
+#    ax2.yaxis.set_label_position('right')
+#    ax2.yaxis.set_minor_locator(AutoMinorLocator(5))    
+    
+    plt.show()
+    
+EPlot()
